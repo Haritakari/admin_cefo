fa99b7c2ed1d0dd145355eef97e9fe4770e88c33
Haritakari <davidsanchezdevesa@gmail.com>
2016-11-18 08:54:03 +0100

    Admin inicial

---
+++ admincefo_juanjdavid/application/controllers/Cursos.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+	
+	class Cursos extends CI_Controller {
+		
+		public function __construct()
+		{
+			parent::__construct();
+			$this->load->model('cursModel');
+		}
+		
+		public function llistar($p=1,$f=10){
+			$curso=new CursModel();
+			$cursos=$curso->llistar($p,$f);
+			
+			$numpag=$curso->calc_query();
+			$numpag=ceil($numpag/$f);
+			
+			
+			$data['numpag']=$numpag;
+			$data['p']=$p;
+			$data['cursos']=$cursos;
+			$data['usuario']=Login::getUsuario();
+			$this->load->view('templates/header', $data);
+			$this->load->view('cursos/veure', $data);
+			$this->load->view('templates/footer', $data);
+		}
+		public function curs($id){
+			$curso=new CursModel();
+			$curso=$curso->getCurs($id);
+			
+			$data['curso']=$curso;
+			$data['usuario']=Login::getUsuario();
+			$this->load->view('templates/header', $data);
+			$this->load->view('cursos/detall', $data);
+			$this->load->view('templates/footer', $data);
+		}
+		public function curs($id){
+			$curso=new CursModel();
+		}
+	}
+				
