53f5d644e4a0bf56a8898aa70fd8e404ca597e82
Haritakari <davidsanchezdevesa@gmail.com>
2016-11-22 19:53:36 +0100

    detal amb erros de  noms (corretgit)

--- admincefo_juanjdavid/application/views/usuario/detall.php
+++ admincefo_juanjdavid/application/views/usuario/detall.php
@@ -1,19 +1,19 @@
 	<div class="cont">
 	<h2>Detalls alumnes</h2>
-		<label class="det">Codi del curs:</label><span>	<?php echo $alumne[0]->nom; echo $alumne[0]->cognom1; echo $alumne[0]->cognom2 ;?></span><br/>
-		<label class="det">Codi del curs:</label><span>	<?php echo $alumne[0]->dni;?></span><br/>
-		<label class="det">Hores totals</label>	<span><?php echo $alumne[0]->estudis;?></span><br/>
-		<label class="det">Data de inici del curs:</label><span>	<?php echo $alumne[0]->data_naixement;?></span><br/>
-		<label class="det">Data fi del curs:</label>	<span><?php echo $alumne[0]->situacio_laboral;?></span><br/>
-		<label class="det">Quin horari tindra:</label><span>	<?php echo $alumne[0]->prestacio;?></span><br/>
-		<label class="det">Mati o tarda?:</label><span>	<?php echo $alumne[0]->telefon_mobil;?></span><br/>
-		<label class="det">Quina graduació te:</label><span>	<?php echo $alumne[0]->telefon_fix;?></span><br/>
-		<label class="det">requisits per poder accedir:</label>	<span><?php echo $alumne[0]->email;?></span><br/>
+		<label class="det">Nom complert:</label><span>	<?php echo $alumne[0]->nom; echo" ". $alumne[0]->cognom1; echo" ". $alumne[0]->cognom2 ;?></span><br/>
+		<label class="det">DNI:</label><span>	<?php echo $alumne[0]->dni;?></span><br/>
+		<label class="det">Estudis</label>	<span><?php echo $alumne[0]->estudis;?></span><br/>
+		<label class="det">Data de Naixement:</label><span>	<?php echo $alumne[0]->data_naixement;?></span><br/>
+		<label class="det">Situacio laboral:</label>	<span><?php echo $alumne[0]->situacio_laboral;?></span><br/>
+		<label class="det">Prestacio:</label><span>	<?php echo $alumne[0]->prestacio;?></span><br/>
+		<label class="det">Telefon mobil:</label><span>	<?php echo $alumne[0]->telefon_mobil;?></span><br/>
+		<label class="det">Telefon fixe:</label><span>	<?php echo $alumne[0]->telefon_fix;?></span><br/>
+		<label class="det">Email:</label>	<span><?php echo $alumne[0]->email;?></span><br/>
 		
 		<a class="botoncin bo1 bot2" href="<?php echo base_url()?>/index.php/usuario/llistar">Tornar a llistat alumnes</a>
 		
 	</div>
-	<div class="cont">
+	<div class="content2">
 		<h2>Cursos als que s'ha preinscrit</h2>
 		
 		
