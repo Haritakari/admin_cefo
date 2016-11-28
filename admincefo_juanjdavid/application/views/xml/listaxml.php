80c0025f60ea8abf81886c8c54352457884afbc0
juanjo <juanjo@juanjo-HP-EliteBook-8440p>
2016-11-28 21:17:57 +0100

    Final noche 28/11/2016

---
+++ admincefo_juanjdavid/application/views/xml/listaxml.php
@@ -0,0 +1,10 @@
+<?php 
+	//cambiando la cabecera hacemos que se pueda descargar el fichero xml
+	//header('Content-type:text/xml; charset=utf-8');
+	header('Content-Disposition:attachment ; filename=motos.xml');
+	
+	require_once 'model/Moto.php';
+	require_once 'libraries/Database.php';
+	
+	echo Moto::toXML(Moto::getMotos());
+?>
