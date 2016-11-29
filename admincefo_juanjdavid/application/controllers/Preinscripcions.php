959fe5e4d27dc9705ce2eac03f24fa320c855690
Haritakari <davidsanchezdevesa@gmail.com>
2016-11-29 20:25:49 +0100

    varis

--- admincefo_juanjdavid/application/controllers/Preinscripcions.php
+++ admincefo_juanjdavid/application/controllers/Preinscripcions.php
@@ -235,6 +235,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						$alumne=new UsuarioModel();
 						$alumne->id=$v->id_usuari;
 						$alumne=$alumne->getUsuario2();
+						$alumne[0]->data_hora=$v->data_hora;
 						$alusubs[]=$alumne[0];
 					}
 				}
