228b6e3e2cc26ba3c666d2a35b010311f229ff45
juanjo <juanjo.guardiola@gmail.com>
2016-11-29 08:35:36 +0100

    petada

--- admincefo_juanjdavid/application/libraries/Templ.php
+++ admincefo_juanjdavid/application/libraries/Templ.php
@@ -55,7 +55,7 @@
 			?>
 			<footer>
 					<br/>
-					<span><a href="cefo@......com ">cefo@.....com</span><span> Aquesta pagina es un projecte academic de  : </span> <span> David Sanchez y Juanjo guardiola</a></span>
+					<span><a href="cefo@......com "></a>cefo@.....com</span><span> Aquesta pagina es un projecte academic de  : </span> <span> David Sanchez y Juanjo guardiola</span>
 			</footer>
 
   </body>
