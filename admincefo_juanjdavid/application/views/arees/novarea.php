f6c08b885676ae14592a0eb69524c7633514fc09
Haritakari <davidsanchezdevesa@gmail.com>
2016-11-29 17:36:56 +0100

    varis

--- admincefo_juanjdavid/application/views/arees/novarea.php
+++ admincefo_juanjdavid/application/views/arees/novarea.php
@@ -3,7 +3,7 @@
 		<form method="post" autocomplete="off">
 			<label>Nom del Area:</label>
 			<input type="text" name="nom" required="required" value=""/><br/><br/>
-			<a class="botoncin bo1" onclick='window.history.back()'>Tornar enrere</a>
+			<a class="botoncin bo1" href="<?php echo base_url()?>/index.php/arees/llistar">Tornar enrere</a>
 			<input class="botoncin bo3" type="submit" name="crear" value="Crear"/><br/>
 		</form>
 	</div>
